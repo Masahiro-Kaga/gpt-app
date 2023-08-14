c2ab66895bea97da826b97bed991a37a67865d38
Masahiro Kaga <masahiro.kaga@bittreo.com>
2023-08-13 21:55:08 -0700

    Test RestAPI Post method

--- src/client/components/ImageGeneration/ImageGenerator.tsx
+++ src/client/components/ImageGeneration/ImageGenerator.tsx
@@ -1,27 +1,36 @@
 import { Button } from "@mui/material";
-import Slider from "@mui/material/Slider"
+import Slider from "@mui/material/Slider";
 import React from "react";
+import axios from "axios";
 
 const ImageGenerator = () => {
-    const getImages = async () => {
-        try {
-          const options = {};
-        } catch (error) {}
-      };
-      return (
-        <>
-          <input className="border-solid"></input>
-          <button className="text-[#50d71e]">Create</button>
-          <Button variant="outlined">Outlined</Button>
-          <Slider
-            size="small"
-            defaultValue={70}
-            aria-label="Small"
-            valueLabelDisplay="auto"
-          />
-        </>
-      );
-    
-}
+  const getImages = async () => {
+    try {
+      // const options = {
 
-export default ImageGenerator
+      // };
+      const data = { key1: "Value1", key2: "Value2" };
+      const response = await axios.post("http://localhost:8000/imageGenerator/images", data);
+      console.log("Clicked!");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <>
+      <input className="border-solid"></input>
+      <button className="text-[#50d71e]" onClick={getImages}>
+        Create
+      </button>
+      <Button variant="outlined">Outlined</Button>
+      <Slider
+        size="small"
+        defaultValue={70}
+        aria-label="Small"
+        valueLabelDisplay="auto"
+      />
+    </>
+  );
+};
+
+export default ImageGenerator;
--- src/server/routers/imageGenerator/index.ts
+++ src/server/routers/imageGenerator/index.ts
@@ -9,13 +9,15 @@ const openai = new OpenAIApi(configuration);
 
 router.post("/images", async(req:Request,res:Response)=>{
     try {
-        const response = await openai.createImage({
-            prompt: "A cute baby sea other",
-            n: 2,
-            size: "1024x1024"
-        });
-        console.log(response);
-        res.send(response.data.data);
+        // const response = await openai.createImage({
+        //     prompt: "A cute baby sea other",
+        //     n: 2,
+        //     size: "1024x1024"
+        // });
+        // console.log(response);
+        // res.send(response.data.data);
+        console.log(req.body);
+        console.log("Success Post");
     } catch (error) {
         console.error(error)
     }
