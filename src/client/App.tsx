b8b5320a6a5e248e8389a3b8b0b67a20fdc9cc12
Masahiro Kaga <masahiro.kaga@bittreo.com>
2023-08-13 21:20:18 -0700

    Set router for Single Page Application (SPA)

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import ImageGeneratorPage from './pages/ImageGeneratorPage';
-import Header from './components/common/Header';
-import Footer from './components/common/Footer';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ImageGeneratorPage from "./pages/ImageGeneratorPage";
+import GptHandlePage from "./pages/GptHandlePage";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
 
 function App() {
   return (
-    <div>
+    <Router>
       <Header></Header>
-      <ImageGeneratorPage></ImageGeneratorPage>
+      <Routes>
+        <Route path="/image-generation" Component={ImageGeneratorPage} />
+        <Route path="/gpt-handler" Component={GptHandlePage} />
+      </Routes>
       <Footer></Footer>
-    </div>
+    </Router>
   );
 }
 
--- src/client/components/common/Header.tsx
+++ src/client/components/common/Header.tsx
@@ -2,7 +2,8 @@ import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button"
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
@@ -12,8 +13,8 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             ヘッダー
           </Typography>
-          <Button color="secondary">Image Generate</Button>
-          <Button color="secondary">GPT Handler</Button>
+          <Button color="secondary" component={Link} to="/image-generation">Image Generate</Button>
+          <Button color="secondary" component={Link} to="/gpt-handler">GPT Handler</Button>
         </Toolbar>
       </AppBar>
     </Box>
