ea0cd320ed65082d5926f000a66f35c5fe7cf4fa
masa <masahiro41802891@gmail.com>
2023-09-27 19:02:06 -0700

    Installed crypt function for the user password

--- src/server/models/User.model.ts
+++ src/server/models/User.model.ts
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 const { Schema } = mongoose;
+
+const SALT_WORK_FACTOR = 10;
 
 const UserSchema = new Schema({
   username: {
@@ -10,6 +13,26 @@ const UserSchema = new Schema({
     required: true,
     type: String,
   },
+});
+
+// // This使っているので、アロー関数は無理。
+UserSchema.pre("save", async function userPasswordHash(next) {
+  const user = this;
+  if (user.isNew || user.isModified("password")) {
+    try {
+      const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+      const hash = await bcrypt.hash(user.password, salt);
+      user.password = hash;
+      console.log(test)
+      next();
+      // https://www.youtube.com/watch?v=pEbA46E7c7o
+    } catch (err) {
+      console.error(err);
+      next();
+    }
+  } else {
+    next();
+  }
 });
 
 export default mongoose.model("User", UserSchema);
